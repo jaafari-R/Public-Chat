dbddefc55d290039f1abed94e05c0603902df478
r i d <jaafariramadan@gmail.com>
2023-07-07 05:55:45 +0300

    Added Initial React Components and Routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,25 @@
 import './App.css';
 
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+
+import Signup from './components/Signup';
+import Signin from './components/Signin';
+import Chat from './components/Chat';
+
 function App() {
   return (
-    <div className="App">
-      App
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          {/* SignUp */}
+          <Route path="/signup" Component={Signup} />
+          {/* SignIn */}
+          <Route path="signin" Component={Signin} />
+          {/* Chat */}
+          <Route path="/" Component={Chat} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
