064f3c8d36b3bb7a114f21efbb82fe8c46201713
r i d <jaafariramadan@gmail.com>
2023-07-11 02:02:47 +0300

    Added the general layout of the Chat page

--- frontend/src/components/Chat.tsx
+++ frontend/src/components/Chat.tsx
@@ -1,14 +1,37 @@
-import { Navigate } from 'react-router-dom';
-import { Username } from '../interfaces/appProps';
 import './Chat.css';
 
+import { Navigate } from 'react-router-dom';
+import { Dispatch, SetStateAction, useState } from 'react';
+
+import { Username } from '../interfaces/appProps';
+import Message from './Message';
+
 function Chat( props: Username ) {
+  const [msg, setMsg] = useState('')
+
   const username = props.loggedUsername;
 
+  const handleChange = (setState: Dispatch<SetStateAction<string>>) => (e: React.ChangeEvent<HTMLInputElement>) => {
+    setState(e.target.value);
+  }
+
   return (
-    <div> 
+    <div className='chat'> 
+      {/* Go to sign in page when not authenticated */}
       {username === '' && <Navigate to='/signin'/>}
-      {username} 
+
+      <header className='chat-userTag'>
+        <h2>{username}</h2>
+      </header>
+
+      {/* Messages Components */}
+      <Message />
+      <Message />
+      <Message />
+
+      <form action="">
+        <input value={msg} onChange={handleChange(setMsg)} type="text" placeholder='Send a message'/>
+      </form>
     </div>
   )
 }
